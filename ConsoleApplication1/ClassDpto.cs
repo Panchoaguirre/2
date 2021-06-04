6f343ae885a0324ec4e0b4a35005aa5d7d0a42ee
Panchoaguirre <85365968+Panchoaguirre@users.noreply.github.com>
2021-06-04 16:03:53 -0400

    Add files via upload

---
+++ ConsoleApplication1/ClassDpto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miclase2
+{
+    class ClassDpto
+    {
+        //los atributos
+        public int numero;
+        public string torre;
+        public int piezas;
+        public int baños;
+        //los metodos
+        
+        public ClassDpto(int numero, string torre, int piezas, int baños)
+        {
+            this.numero = numero;
+            this.torre = torre;
+            this.piezas = piezas;
+            this.baños = baños;
+
+        }
+
+        public ClassDpto()
+        {
+        }
+        
+        public void ModificarDpto(int n,string t, int p, int b)
+        {
+            this.numero = n;
+            this.torre = t;
+            this.piezas = p;
+            this.baños = b;
+        }
+       
+    
+     }
+
+
+    }
+    
---
+++ ConsoleApplication1/ClassPersonal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miclase2
+{ 
+    class ClassPersonal
+    {
+        //los atributos
+       
+        public string nombres;
+        public string apellidos;
+        public int rut;
+        public int edad;
+        public int cargo;
+        //los metodos
+        // cargo 1 para aseo, 2 para supervisor
+        public ClassPersonal(string nombres, string apellidos, int rut, int edad, int cargo)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.rut = rut;
+            this.edad = edad;
+            this.cargo = cargo;
+        }
+
+        public ClassPersonal()
+        {
+        }
+        public void ModificarPerso(string nom,string a,int r,int e, int c)
+        {
+            this.nombres = nom;
+            this.apellidos = a;
+            this.rut = r;
+            this.edad = e;
+            this.cargo = c;
+        }
+    }
+}
